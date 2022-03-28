35c20ff188d7667c42ae253936874967d6401bb1
Saurav-Suman-TM <sauravsuman001@hotmail.com>
2022-03-28 10:04:19 +0530

    gets user data in profile view model through session manager

--- app/src/main/java/io/demoapps/comic_characters/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/io/demoapps/comic_characters/ui/main/profile/ProfileFragment.kt
@@ -1,13 +1,16 @@
 package io.demoapps.comic_characters.ui.main.profile
 
 import android.os.Bundle
+import android.service.autofill.UserData
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import dagger.android.support.DaggerFragment
 import io.demoapps.comic_characters.R
+import io.demoapps.comic_characters.models.User
+import io.demoapps.comic_characters.ui.auth.AuthResource
 import io.demoapps.comic_characters.viewmodel.ViewModelProvidersFactory
 import javax.inject.Inject
 
@@ -22,11 +25,42 @@ class ProfileFragment : DaggerFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Toast.makeText(activity, "Profile fragment", Toast.LENGTH_SHORT).show()
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         profileViewModel = ViewModelProvider(this, providersFactory).get(ProfileViewModel::class.java)
+        subscribeObserver()
     }
+
+    private fun subscribeObserver(){
+        profileViewModel.getAuthenticatedUser().removeObservers(viewLifecycleOwner)
+        profileViewModel.getAuthenticatedUser().observe(viewLifecycleOwner, object : Observer<AuthResource<User>>{
+            override fun onChanged(user: AuthResource<User>?) {
+                if(user != null){
+                    when(user.status){
+                        AuthResource.AuthStatus.AUTHENTICATED -> {
+                            setUserDetails(user.data)
+                        }
+                        AuthResource.AuthStatus.ERROR -> {
+                            setUserError(user.message)
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setUserDetails(user: User?){
+        if(user != null){
+
+        }
+    }
+
+    private fun setUserError(message: String?){
+        if(message != null){
+
+        }
+    }
+    
 }
--- app/src/main/java/io/demoapps/comic_characters/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/io/demoapps/comic_characters/ui/main/profile/ProfileViewModel.kt
@@ -1,7 +1,17 @@
 package io.demoapps.comic_characters.ui.main.profile
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import io.demoapps.comic_characters.SessionManager
+import io.demoapps.comic_characters.models.User
+import io.demoapps.comic_characters.ui.auth.AuthResource
 import javax.inject.Inject
 
-class ProfileViewModel @Inject constructor(): ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val sessionManager: SessionManager
+): ViewModel() {
+
+    fun getAuthenticatedUser(): LiveData<AuthResource<User>> {
+        return sessionManager.getAuthUser()
+    }
 }
